005e119a488bc7cdc5aa35c909c3c565dcd0f88f
Mateo <133899684+Mateoavr@users.noreply.github.com>
2025-05-27 23:37:38 -0400

    Delete msvc.gestionVenta directory

--- msvc.gestionVenta/src/main/java/Repository/gestionVentaRepository.java
+++
@@ -1,9 +0,0 @@
-package Repository;
-
-import models.Venta;
-
-import java.util.List;
-
-public interface gestionVentaRepository extends JpaRepository<Venta, Long> {
-    List<Venta> findByUsuarioId(Long usuarioId);
-}
