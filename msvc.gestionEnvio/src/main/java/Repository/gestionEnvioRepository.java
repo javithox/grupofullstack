c2ed35054a2a32f79488ccdf0103e2eb9be3bc4f
Mateo <133899684+Mateoavr@users.noreply.github.com>
2025-05-27 23:37:58 -0400

    Delete msvc.gestionEnvio directory

--- msvc.gestionEnvio/src/main/java/Repository/gestionEnvioRepository.java
+++
@@ -1,9 +0,0 @@
-package Repository;
-
-import models.Envio;
-
-import java.util.List;
-
-public interface gestionEnvioRepository extends JpaRepository<Envio, Long> {
-    List<Envio> findByVentaId(Long ventaId);
-}
