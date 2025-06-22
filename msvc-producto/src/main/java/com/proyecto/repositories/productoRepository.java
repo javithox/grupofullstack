3d05938000cf7429c8eb04787329820c6b17fc61
javier <ja.quirogqb@duocuc.cl>
2025-06-21 20:05:59 -0400

    commit

--- msvc-producto/src/main/java/com/proyecto/repositories/productoRepository.java
+++ msvc-producto/src/main/java/com/proyecto/repositories/productoRepository.java
@@ -1,6 +1,7 @@
 package com.proyecto.repositories;
 
-import com.perfulandia.msvc.producto.models.Producto;
+
+import com.proyecto.models.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface productoRepository extends JpaRepository<Producto,Long> {
