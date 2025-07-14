eff5e577862c32dcea6a967e83990d6dcb400467
javier <ja.quirogab@duocuc.cl>
2025-07-14 07:30:49 -0400

    commit

---
+++ msvc.gestionTienda/src/main/java/com/grupoFullStack/msvc/gestionTienda/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.grupoFullStack.msvc.gestionTienda.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        Contact contact = new Contact();
+        contact.setName("Javier Quiroga");
+        contact.setEmail("ja.quirogab@duocuc.cl");
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API - MSVC - tienda")
+                        .version("1.0.2")
+                        .description("Este es el microservicio de tienda, con el puedes realizar todas las consultas" +
+                                " CRUD que necesites")
+                        .contact(contact)
+                        .summary("esto es una api dentro de un proyecto de MSVC")
+                );
+    }
+}
